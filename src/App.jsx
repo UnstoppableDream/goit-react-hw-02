a54bb4de4fc84dc6d2546d7dd60a0673f675e0d9
Adriano <151621951+UnstoppableDream@users.noreply.github.com>
2024-10-08 09:27:47 +0200

    #1

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Description from "./components/Description/Description";
+import Feedback from "./components/Feedback/Feedback";
+import Options from "./components/Options/Options";
+import Notification from "./components/Notifications/Notifications";
+
+const App = () => {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  });
+
+  useEffect(() => {
+    const savedFeedback = JSON.parse(localStorage.getItem("feedback")) || {
+      good: 0,
+      neutral: 0,
+      bad: 0
+    };
+    setFeedback(savedFeedback);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1
+    }));
+  };
+
+  const resetFeedback = () => {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positivePercentage = totalFeedback
+    ? Math.round((feedback.good / totalFeedback) * 100)
+    : 0;
+
+  return (
+    <div className="app-container">
+      <Description />
+      <Options
+        updateFeedback={updateFeedback}
+        totalFeedback={totalFeedback}
+        resetFeedback={resetFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback
+          feedback={feedback}
+          totalFeedback={totalFeedback}
+          positivePercentage={positivePercentage}
+        />
+      ) : (
+        <Notification message="No feedback given" />
+      )}
+    </div>
+  );
+};
+
+export default App;
